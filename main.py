004fb2841065106a5283c3764f6298bdfcff2f13
Grayson Miller <grayson.miller124@gmail.com>
2017-01-05 14:06:52 -0800

    Fairly working state. Code needs to be grouped and organized, lots of code is used repetitively. This indicates grouping it together, such as code to enable/disable debug states, reset the interpreter, etc. Cleaning required, but for the most part the debugger is working well. I need a better way to display the past states.

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import tkinter as tk
 import sys
 
 
+# TODO: BF breakpoints, debug run should full run and only stop at breaks then start button for what debug run does now
 # TODO: Auto-step pause, finish, etc.
 # TODO: Time Taken
 # TODO: Display filename loaded
@@ -50,19 +51,19 @@ class BrainfuckApp(tk.Frame):
         self.button_frame = tk.LabelFrame(self, text="")
         self.button_frame.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
         self.text_frame = tk.LabelFrame(self, text="Input / Source")
-        self.text_frame.grid(row=1, column=0, columnspan=4, padx=10, sticky=tk.E+tk.W+tk.S+tk.N)
+        self.text_frame.grid(row=1, column=0, columnspan=3, padx=10, sticky=tk.E+tk.W+tk.S+tk.N)
         self.text_frame.rowconfigure(0, weight=1)
         self.text_frame.columnconfigure(0, weight=1)
         self.text_frame.columnconfigure(1, weight=1)
         self.output_frame = tk.LabelFrame(self, text="Output")
-        self.output_frame.grid(row=2, column=0, padx=10, sticky=tk.W)
-        self.debug_frame = tk.LabelFrame(self, text="Tape")
-        self.debug_frame.grid(row=3, column=0)
+        self.output_frame.grid(row=2, column=0, padx=10, sticky=tk.W, columnspan=3)
+        self.debug_frame = tk.LabelFrame(self, text="Debug Info")
+        self.debug_frame.grid(row=3, column=0, padx=10, pady=10)
         self.debug_button_frame = tk.LabelFrame(self, text="")
         self.debug_button_frame.grid(row=3, column=2, padx=10, pady=10)
         # Button frame for basic operations.
         self.run = tk.Button(self.button_frame, text="run", command=self.bf_default_run)
-        self.run.grid(row=0, column=0)
+        self.run.grid(row=0, column=0, pady=(5,5), padx=(5,0))
         self.reset_btn = tk.Button(self.button_frame, text="reset", command=self.reset)
         self.reset_btn.grid(row=0, column=1, padx=(0,20))
         self.load = tk.Button(self.button_frame, text="load", command=self.load_bf_file)
@@ -70,18 +71,20 @@ class BrainfuckApp(tk.Frame):
         self.save = tk.Button(self.button_frame, text="save", command=self.save_bf_file)
         self.save.grid(row=0, column=3)
         self.clear = tk.Button(self.button_frame, text="clear", command=self.clear_inputs)
-        self.clear.grid(row=0, column=4, padx=(20,0))
+        self.clear.grid(row=0, column=4, padx=(20,5))
         # Debug frame for displaying the interpreter state.
+        self.states = tk.Text(self.debug_frame, height=2, width=97, background="#CCCCCC", wrap=tk.NONE)
+        self.states.grid(row=0, column=0, padx=5, pady=5)
         self.state_memory = tk.Text(self.debug_frame, background=self.master.cget("background"),
                                     borderwidth=0, height=1, width=30)
-        self.state_memory.grid(row=0, column=0)
+        self.state_memory.grid(row=0, column=1)
         self.tape_text = tk.Text(self.debug_frame, background="#CCCCCC", height=1, width=127, state=tk.DISABLED)
-        self.tape_text.grid(row=1, column=0)
+        self.tape_text.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
         # Debug Button frame for the interpreter operations.
         self.debug = tk.Button(self.debug_button_frame, text="debug", command=self.bf_debug_run)
-        self.debug.grid(row=0, column=0, padx=(0,20))
+        self.debug.grid(row=0, column=0, padx=(5,20),pady=5)
         self.auto_step_backward = tk.Button(self.debug_button_frame, text="←", command=self.bf_auto_step_backward)
-        self.auto_step_backward.grid(row=0, column=1)
+        self.auto_step_backward.grid(row=0, column=1, )
         self.step_backward = tk.Button(self.debug_button_frame, text="◄", command=self.bf_step_backward)
         self.step_backward.grid(row=0, column=2)
         self.pause_btn = tk.Button(self.debug_button_frame, text="●", command=self.toggle_pause)
@@ -89,9 +92,9 @@ class BrainfuckApp(tk.Frame):
         self.step_forward = tk.Button(self.debug_button_frame, text="►", command=self.bf_step_forward)
         self.step_forward.grid(row=0, column=4)
         self.auto_step_forward = tk.Button(self.debug_button_frame, text="→", command=self.bf_auto_step_forward)
-        self.auto_step_forward.grid(row=0, column=5)
+        self.auto_step_forward.grid(row=0, column=5, padx=(0,5))
         self.hertz_slider = tk.Scale(self.debug_button_frame, from_=1, to_=100, orient=tk.HORIZONTAL)
-        self.hertz_slider.grid(row=1, column=0, columnspan=5)
+        self.hertz_slider.grid(row=1, column=0, columnspan=5, pady=(0,5))
         # Text frame for modifying the source and input.
         self.input = tk.Text(self.text_frame)
         self.input.bind("<Control-r>", lambda _: self.bf_default_run)
@@ -117,9 +120,14 @@ class BrainfuckApp(tk.Frame):
         self.reset()
 
     def reset(self):
-        self.master.after_cancel(self.current_after)
+        if self.current_after is not None:
+            self.master.after_cancel(self.current_after)
         self.current_after = None
 
+        # TODO: This is the reenable that shoudl be in its own function and accompanied by debug dsiable function
+        self.source.configure(state=tk.NORMAL)
+        self.auto_step_forward.configure(state=tk.NORMAL)
+        self.auto_step_backward.configure(state=tk.NORMAL)
         self.source.tag_delete("current_instr")
 
         self.interpreter_states = []
@@ -133,23 +141,39 @@ class BrainfuckApp(tk.Frame):
         self.output.config(state=tk.DISABLED)
 
         if debug:
+            if self.interpreter.running:
+                self.source.configure(state=tk.NORMAL)
             self.source.tag_delete("current_instr")
             self.source.tag_add("current_instr", index1="{}.{}".format(*self.get_line_offset(self.interpreter.i)))
             self.source.tag_config("current_instr", background='green')
-
+            if self.interpreter.running:
+                self.source.configure(state=tk.DISABLED)
         # TODO: Fix for when you hit edges to wrap the visible cells
         self.tape_text.config(state=tk.NORMAL)
         self.tape_text.delete(1.0, tk.END)
         tape_str = self.interpreter.tape.copy()
         tape_str[self.interpreter.tape_pos] = "<{}>".format(tape_str[self.interpreter.tape_pos])
-        tape_str = tape_str[self.interpreter.tape_pos-31:self.interpreter.tape_pos+31]
+        tape_str = tape_str[self.interpreter.tape_pos-32:self.interpreter.tape_pos+32]
         self.tape_text.insert(1.0, tape_str)
         self.tape_text.config(state=tk.DISABLED)
 
-        self.tape_text.config(state=tk.NORMAL)
+        self.state_memory.config(state=tk.NORMAL)
         self.state_memory.delete(1.0, tk.END)
         self.state_memory.insert(1.0, "State Memory: {}KiB".format(sys.getsizeof(self.interpreter_states)/1000))
-        self.tape_text.config(state=tk.DISABLED)
+        self.state_memory.config(state=tk.DISABLED)
+
+        self.states.config(state=tk.NORMAL)
+        self.states.delete(1.0, tk.END)
+        try:
+            state1 = self.interpreter_states[-1]
+            state1["tape"] = list(filter((0).__ne__, state1["tape"]))
+            state2 = self.interpreter_states[-2]
+            state2["tape"] = list(filter((0).__ne__, state2["tape"]))
+            self.states.insert(1.0, "{}\n{}".format(state1, state2))
+        except IndexError:
+            pass
+        self.states.config(state=tk.DISABLED)
+
 
     def get_line_offset(self, ins_pos):
         ins_pos-=1
@@ -169,7 +193,9 @@ class BrainfuckApp(tk.Frame):
 
         return line_count, (ins_pos-(total_len-current_line))
 
-    # TODO: Factor functions that loop separately to not have as much redundant code or unnecessary code running
+    # TODO: Factor functions that loop separately to not have as much redundant code or unnecessary code running...
+    # TODO: Disable all debug buttons in one function and have a reenable function reenable all buttons
+    # ...There are a lot of repetitions in different code blocks, functionalize this.
     def bf_default_run(self):
         if not self.interpreter.running:
             self.interpreter.update(source_string=self.source.get(1.0, tk.END),
@@ -181,6 +207,7 @@ class BrainfuckApp(tk.Frame):
             self.interpreter.step()
         else:
             self.update_gui_bf_state()
+            self.source.configure(state=tk.NORMAL)
             self.interpreter.running = False
 
         if self.interpreter.running and not self.pause:
@@ -190,6 +217,7 @@ class BrainfuckApp(tk.Frame):
 
 
     def bf_debug_run(self):
+        self.source.configure(state=tk.DISABLED)
         self.source.tag_delete("current_instr")
 
         if not self.interpreter.running:
@@ -204,13 +232,20 @@ class BrainfuckApp(tk.Frame):
             self.bf_debug_run()
 
     # TODO: Thread the mainloop/etc on main thread and interpreter/all inited functions in secondary
+    # TODO: Separate looping parts from init parts for all buttons
     def bf_auto_step_forward(self):
         step_hertz = self.hertz_slider.get()
 
         self.bf_step_forward()
 
         if self.interpreter.running and not self.pause:
+            self.auto_step_forward.configure(state=tk.DISABLED)
+            self.auto_step_backward.configure(state=tk.DISABLED)
             self.current_after = self.master.after(int(1000 / step_hertz), self.bf_auto_step_forward)
+        else:
+            self.auto_step_backward.configure(state=tk.NORMAL)
+            self.auto_step_forward.configure(state=tk.NORMAL)
+            self.source.configure(state=tk.NORMAL)
 
     def bf_auto_step_backward(self):
         step_hertz = self.hertz_slider.get()
@@ -218,10 +253,15 @@ class BrainfuckApp(tk.Frame):
         self.bf_step_backward()
 
         if self.interpreter.running and not self.pause:
+            self.auto_step_forward.configure(state=tk.DISABLED)
+            self.auto_step_backward.configure(state=tk.DISABLED)
             self.current_after = self.master.after(int(1000 / step_hertz), self.bf_auto_step_backward)
         else:
+            self.auto_step_backward.configure(state=tk.NORMAL)
+            self.auto_step_forward.configure(state=tk.NORMAL)
             self.toggle_pause()
 
+    #TODO: if you modify source while running it'll run the loaded source...lock source input while debugging
     def bf_step_forward(self):
         if self.interpreter.running:
             self.interpreter_states.append(self.interpreter.save_state())
@@ -251,7 +291,7 @@ class BrainfuckApp(tk.Frame):
                 self.bf_debug_run()
 
     def load_bf_file(self):
-        filename = filedialog.askopenfilename()
+        filename = filedialog.askopenfilename(defaultextension=".bf", filetypes=(("Brainfuck Script", "*.bf"),("All Files", "*.*")))
 
         if filename == '':
             return
@@ -264,7 +304,7 @@ class BrainfuckApp(tk.Frame):
                 self.source.insert(tk.END, line)
 
     def save_bf_file(self):
-        filename = filedialog.asksaveasfilename(defaultextension="bf")
+        filename = filedialog.asksaveasfilename(defaultextension=".bf", filetypes=(("Brainfuck Script", "*.bf"),("All Files", "*.*")))
         if filename == '':
             return
 
@@ -314,9 +354,9 @@ class BrainfuckInterpreter:
         self.running = False
 
 # TODO: threading
-# TODO: Fix glitches to run Esolang examples
-# TODO: Modify to toggle unbounded, bounded, no wrap, wrap, EOF, etc wrap cells etc
-
+# TODO: Fix glitches to run Esolang examples (Not really glitches, just different interpreter types)
+# TODO: Modify to toggle unbounded, bounded, no wrap, wrap, EOF, etc wrap cells etc Have a new command like # to...
+# ... denote all this so individual programs can specify their needs.
     def step(self):
         if self.i < len(self.source_string):
             if self.source_string[self.i] == '+':
