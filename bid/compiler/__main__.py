d3a5d8a025e758084a2cd4d8eb2c6a430e9b1fbc
Grayson Miller <grayson.miller124@gmail.com>
2024-01-29 11:03:08 -0700

    argparser

--- bid/compiler/__main__.py
+++ bid/compiler/__main__.py
@@ -1,52 +1,72 @@
 import os
 import subprocess
+import argparse
 
 from .languages.c import BfToC
 from .languages.asm import BfToNASM
 from ..utils import load_bf
 
 
-def c_compile_write_and_run(bf_src, BfO, output_dir="output"):
-    print("C")
-    os.makedirs(f"{output_dir}/build", exist_ok=True)
-    with open(f"{output_dir}/test.c", "w") as f:
-        f.write(BfO.compile(bf_src))
-
-    subprocess.run(["gcc", f"{output_dir}/test.c", "-o", f"{output_dir}/build/test_c"])
-    subprocess.run([f"{output_dir}/build/test_c"])
-    print()
-
-
-def nasm_compile_write_and_run(bf_src, BfO, output_dir="output"):
-    print("ASM")
-    os.makedirs(f"{output_dir}/build", exist_ok=True)
-    with open(f"{output_dir}/test.asm", "w") as f:
-        f.write(BfO.compile(bf_src))
-
-    subprocess.run(
-        [
-            "nasm",
-            "-f",
-            "elf64",
-            f"{output_dir}/test.asm",
-            "-o",
-            f"{output_dir}/build/test_nasm.o",
-        ]
+def argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="input file or string", required=True)
+    parser.add_argument(
+        "-p", "--programs-dir", help="programs directory", default="programs"
     )
-    subprocess.run(
-        ["ld", f"{output_dir}/build/test_nasm.o", "-o", f"{output_dir}/build/test_nasm"]
+    parser.add_argument("-o", "--output", help="output directory", default="output")
+    parser.add_argument("-l", "--language", help="output language", default="c")
+    parser.add_argument(
+        "-c",
+        "--compile",
+        help="compile to executable",
+        default=False,
+        action="store_true",
     )
-    subprocess.run([f"{output_dir}/build/test_nasm"])
-    print()
+    parser.add_argument(
+        "-r", "--run", help="run executable", default=False, action="store_true"
+    )
+    return parser.parse_args()
 
 
 def main():
-    os.makedirs("output/build", exist_ok=True)
+    args = argparser()
+    possible_name, bf_src = load_bf(args.input, args.programs_dir)
+    output_dir = args.output
+    language = args.language
+    print(f"Input: {bf_src}")
 
-    bf_src = load_bf("hello_world")
+    if args.language in ["c", "py", "asm"]:
+        extension = args.language
+        build_dir = os.path.join(output_dir, "build")
+        os.makedirs(build_dir, exist_ok=True)
+        output_name = possible_name or f"output_{language}_{len(bf_src)}"
+        output_file = os.path.join(output_dir, f"{output_name}.{extension}")
+        compiled_file = os.path.join(build_dir, f"{output_name}_{language}")
 
-    c_compile_write_and_run(bf_src, BfToC())
-    nasm_compile_write_and_run(bf_src, BfToNASM())
+        if args.compile or args.run:
+            if language == "c":
+                BfO = BfToC()
+            elif language == "asm":
+                BfO = BfToNASM()
+            else:
+                raise NotImplementedError("language not implemented")
+
+            with open(output_file, "w") as f:
+                f.write(BfO.compile(bf_src))
+
+        if args.run:
+            if language == "c":
+                subprocess.run(["gcc", output_file, "-o", compiled_file])
+            elif language == "asm":
+                subprocess.run(
+                    ["nasm", "-f", "elf64", output_file, "-o", f"{compiled_file}.o"]
+                )
+                subprocess.run(["ld", f"{compiled_file}.o", "-o", compiled_file])
+
+        subprocess.run([compiled_file])
+        print(f"Output: {output_file}", f"Compiled: {compiled_file}", sep="\n")
+    else:
+        print("Invalid language")
 
 
 if __name__ == "__main__":
--- bid/utils.py
+++ bid/utils.py
@@ -1,3 +1,14 @@
-def load_bf(name):
-    with open("programs/" + name + ".bf") as f:
-        return f.read()
+import os
+
+
+def load_bf(filename_or_bf_str, programs_dir="programs") -> tuple[str, str]:
+    filename = f"{programs_dir}/{filename_or_bf_str}.bf"
+
+    print(filename)
+    if os.path.isfile(filename):
+        name = filename_or_bf_str
+        with open(filename) as f:
+            return name, f.read()
+    else:
+        print("file does not exist")
+        return None, filename_or_bf_str
